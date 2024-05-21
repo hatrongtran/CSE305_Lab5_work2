d49c3c500c1fd3e3a4040dcce722f4f3457d76f4
Tran Trong Ngoc Ha <87688322+hatrongtran@users.noreply.github.com>
2024-05-21 15:22:54 +0700

    add class Document

---
+++ Document.java
@@ -0,0 +1,9 @@
+public class Document {
+    private String extention;
+    private String encryption;
+
+    public Document(String extention, String encryption) {
+        this.extention = extention;
+        this.encryption = encryption;
+    }
+}
