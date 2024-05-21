1a7f6d9930a425843ea2a71e4d8c5760c0ba8166
Tran Trong Ngoc Ha <87688322+hatrongtran@users.noreply.github.com>
2024-05-21 15:22:41 +0700

    add class confidentialDoc

---
+++ confidentialDoc.java
@@ -0,0 +1,19 @@
+public class confidentialDoc implements DocumentBuilder {
+    private String extention;
+    private String encryption;
+    @Override
+    public void SetEncryption(String encryption) {
+      this.encryption=encryption;
+        
+    }
+    @Override
+    public void SetExtention(String extention) {
+       this.extention=extention;
+        
+    }
+    @Override
+    public Document buildDocument() {
+       
+        return null;
+    }
+}
